0a39f2d3d5a093ecd991b0c9e4f7fc59341815b2
Ondřej Novák <ondra-novak@email.cz>
2025-02-20 19:20:00 +0100

    prepare linux build target Ubuntu 22, x64

--- platform/linux/skeldal.sh
+++ platform/linux/skeldal.sh
@@ -2,7 +2,7 @@
 
 temp_file=$(mktemp /tmp/skeldal.XXXXXX.log)
 
-./skeldal > "$temp_file" 2>&1 
+`dirname $0`/skeldal_bin $* > "$temp_file" 2>&1 
 
 exit_code=$?
 
@@ -10,7 +10,7 @@ if [ $exit_code -ne 0 ]; then
     error_message=$(cat "$temp_file")
     
     if command -v zenity > /dev/null; then
-        zenity --warning --title="Skeldal ERROR" --text="$error_message"
+        zenity --warning --no-markup --title="Skeldal ERROR" --text="$error_message"
     elif command -v kdialog > /dev/null; then
         kdialog --title "Skeldal ERROR" --error "$error_message"
     elif command -v xmessage > /dev/null; then
@@ -22,5 +22,5 @@ if [ $exit_code -ne 0 ]; then
         cat "$temp_file"
     fi
 fi
-#rm $temp_file
+rm $temp_file
 exit $exit_code
